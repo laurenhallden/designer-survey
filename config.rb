23e09ec3a382e37a167836b6512b37fd02479541
laurenhallden <lhallden@rjmetrics.com>
2019-05-01 16:32:38 -0400

    adding nav and sections

--- config.rb
+++ config.rb
@@ -5,9 +5,6 @@ require 'compass/import-once/activate'
 # Links / Permalink Settings #
 # -------------------------- #
 
-url: 
-baseurl: "/"
-
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
