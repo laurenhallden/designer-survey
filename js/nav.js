84fc6d09261c63410ff21ce21b9e782132cb607b
Lauren Hallden <laurenhallden@gmail.com>
2020-01-07 17:29:19 -0500

    adding color options

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+console.log('Hi');
+var index = $('.subnav .nav-item.active').index();
+var text = $('.subnav .nav-item.active').html();
+console.log(index);
+
+if (index == 0) {
+	$('.subnav').attr('class','navbar subnav lime');
+	$('.content-wrapper').attr('class','content-wrapper lime');
+} else if (index == 1) {
+	$('.subnav').attr('class','navbar subnav green');
+	$('.content-wrapper').attr('class','content-wrapper green');
+} else if (index == 2) {
+	$('.subnav').attr('class','navbar subnav blue');
+	$('.content-wrapper').attr('class','content-wrapper blue');
+} else if (index == 3) {
+	$('.subnav').attr('class','navbar subnav purple');
+	$('.content-wrapper').attr('class','content-wrapper purple');
+} else {
+	$('.subnav').attr('class','navbar subnav red');
+	$('.content-wrapper').attr('class','content-wrapper red');
+}
